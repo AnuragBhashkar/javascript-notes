c07fd8e18a5654bbc127113cda953667f4697010
AnuragBhashkar <anuragbhashkarofficial@gmail.com>
2025-09-06 22:43:11 +0530

    added

--- 01_basics/06_stackHeap.js
+++ 01_basics/06_stackHeap.js
@@ -16,9 +16,10 @@ Stack(copy) vs Heap Memory(reference):-
         - reference of original value so it will directly change the value
 */
 
+//
 //-------------------------------------------------------------------------------------------------//
  let myName = "anurag"; // stored in stack
  let anotherName = myName; // copy of myName is created in stack
  anotherName = "bhashkar"; // changing anotherName will not affect myName
     console.log(myName); // Output: anurag
-    console.log(anotherName); // Output: bhashkar
+    console.log(anotherName); // Output: bhashkar because it is copy of myName
